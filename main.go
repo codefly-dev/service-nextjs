77d658adaa07c4ec7604793754d133010be22e99
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-02-26 08:51:44 -0500

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"embed"
+
+	"github.com/codefly-dev/core/builders"
+
+	basev0 "github.com/codefly-dev/core/generated/go/base/v0"
+
+	"github.com/codefly-dev/core/configurations/standards"
+
+	"github.com/codefly-dev/core/templates"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/codefly-dev/core/agents"
+	"github.com/codefly-dev/core/agents/services"
+	"github.com/codefly-dev/core/configurations"
+	agentv0 "github.com/codefly-dev/core/generated/go/services/agent/v0"
+	"github.com/codefly-dev/core/shared"
+)
+
+// Agent version
+var agent = shared.Must(configurations.LoadFromFs[configurations.Agent](shared.Embed(info)))
+
+var requirements = builders.NewDependencies(agent.Name,
+	builders.NewDependency("service.codefly.yaml"),
+	builders.NewDependency("components", "interfaces", "pages", "styles", "additional.d.ts",
+		"next-env.d.ts", "tsconfig.json", "postcss.config.js", "tailwind.config.js", "package.json").WithPathSelect(shared.NewIgnore("node_modules/*")),
+)
+
+type Settings struct {
+	DeveloperDebug bool `yaml:"debug"` // Developer only
+}
+
+type Service struct {
+	*services.Base
+
+	EnvironmentVariables *configurations.EnvironmentVariableManager
+
+	// Settings
+	*Settings
+	Endpoint *basev0.Endpoint
+}
+
+func (s *Service) GetAgentInformation(ctx context.Context, _ *agentv0.AgentInformationRequest) (*agentv0.AgentInformation, error) {
+	defer s.Wool.Catch()
+
+	readme, err := templates.ApplyTemplateFrom(ctx, shared.Embed(readme), "templates/agent/README.md", s.Information)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &agentv0.AgentInformation{
+		RuntimeRequirements: []*agentv0.Runtime{
+			{Type: agentv0.Runtime_NPM},
+		},
+		Capabilities: []*agentv0.Capability{
+			{Type: agentv0.Capability_BUILDER},
+			{Type: agentv0.Capability_RUNTIME},
+			{Type: agentv0.Capability_HOT_RELOAD},
+		},
+		Languages: []*agentv0.Language{
+			{Type: agentv0.Language_TYPESCRIPT},
+			{Type: agentv0.Language_JAVASCRIPT},
+		},
+		Protocols: []*agentv0.Protocol{
+			{Type: agentv0.Protocol_HTTP},
+		},
+		ReadMe: readme,
+	}, nil
+}
+
+func NewService() *Service {
+	return &Service{
+		Base:     services.NewServiceBase(context.Background(), agent.Of(configurations.ServiceAgent)),
+		Settings: &Settings{},
+	}
+}
+
+func main() {
+	agents.Register(services.NewServiceAgent(agent.Of(configurations.ServiceAgent), NewService()), services.NewBuilderAgent(agent.Of(configurations.RuntimeServiceAgent), NewBuilder()), services.NewRuntimeAgent(agent.Of(configurations.BuilderServiceAgent), NewRuntime()))
+}
+
+func (s *Service) LoadEndpoints(ctx context.Context) error {
+	defer s.Wool.Catch()
+	for _, endpoint := range s.Configuration.Endpoints {
+		endpoint.Application = s.Configuration.Application
+		endpoint.Service = s.Configuration.Name
+		switch endpoint.API {
+		case standards.HTTP:
+			http, err := configurations.NewHTTPApi(ctx, endpoint)
+			if err != nil {
+				return s.Wool.Wrapf(err, "cannot create openapi api")
+			}
+			s.Endpoints = []*basev0.Endpoint{http}
+			continue
+		}
+	}
+	return nil
+}
+
+//go:embed agent.codefly.yaml
+var info embed.FS
+
+//go:embed templates/agent
+var readme embed.FS
---
+++ runtime.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/codefly-dev/core/configurations"
+	"github.com/codefly-dev/core/configurations/standards"
+	basev0 "github.com/codefly-dev/core/generated/go/base/v0"
+
+	"github.com/codefly-dev/core/agents/helpers/code"
+	agentv0 "github.com/codefly-dev/core/generated/go/services/agent/v0"
+	runtimev0 "github.com/codefly-dev/core/generated/go/services/runtime/v0"
+	"github.com/codefly-dev/core/runners"
+	"github.com/codefly-dev/core/shared"
+	"github.com/codefly-dev/core/templates"
+	"github.com/codefly-dev/core/wool"
+)
+
+type Runtime struct {
+	*Service
+	Runner *runners.Runner
+
+	NetworkMappings []*basev0.NetworkMapping
+
+	Port int
+}
+
+func NewRuntime() *Runtime {
+	return &Runtime{
+		Service: NewService(),
+	}
+}
+
+func (s *Runtime) Load(ctx context.Context, req *runtimev0.LoadRequest) (*runtimev0.LoadResponse, error) {
+	defer s.Wool.Catch()
+
+	err := s.Base.Load(ctx, req.Identity, s.Settings)
+	if err != nil {
+		return s.Base.Runtime.LoadError(err)
+	}
+
+	s.EnvironmentVariables = s.LoadEnvironmentVariables(req.Environment)
+
+	err = s.LoadEndpoints(ctx)
+	if err != nil {
+		return s.Base.Runtime.LoadError(err)
+	}
+	s.Wool.Focus("loading runtime", wool.NullableField("endpoints", configurations.MakeEndpointSummary(s.Endpoints)))
+	return s.Base.Runtime.LoadResponse()
+}
+
+func (s *Runtime) Init(ctx context.Context, req *runtimev0.InitRequest) (*runtimev0.InitResponse, error) {
+	defer s.Wool.Catch()
+
+	s.Wool.Focus("initialize runtime", wool.NullableField("dependency endpoints", configurations.MakeEndpointSummary(req.DependenciesEndpoints)))
+
+	s.NetworkMappings = req.ProposedNetworkMappings
+
+	net, err := configurations.GetMappingInstanceFor(s.NetworkMappings, standards.HTTP)
+	if err != nil {
+		return s.Runtime.InitError(err)
+	}
+
+	s.Port = net.Port
+
+	return s.Base.Runtime.InitResponse(s.NetworkMappings)
+}
+
+func (s *Runtime) Start(ctx context.Context, req *runtimev0.StartRequest) (*runtimev0.StartResponse, error) {
+	defer s.Wool.Catch()
+	ctx = s.Wool.Inject(ctx)
+
+	s.Wool.Debug("network mappings", wool.NullableField("other", configurations.MakeNetworkMappingSummary(req.OtherNetworkMappings)))
+
+	envs := s.EnvironmentVariables.GetBase()
+
+	publicNetworkMappings := configurations.ExtractPublicNetworkMappings(req.OtherNetworkMappings)
+
+	s.Wool.Focus("public network mappings", wool.NullableField("public", configurations.MakeNetworkMappingSummary(publicNetworkMappings)))
+
+	endpointEnvs, err := configurations.ExtractEndpointEnvironmentVariables(ctx, publicNetworkMappings)
+	if err != nil {
+		return s.Base.Runtime.StartError(err, wool.InField("converting incoming network mappings"))
+	}
+
+	envs = append(envs, endpointEnvs...)
+
+	restEnvs, err := configurations.ExtractRestRoutesEnvironmentVariables(ctx, publicNetworkMappings)
+	if err != nil {
+		return s.Base.Runtime.StartError(err, wool.InField("converting incoming network mappings"))
+	}
+
+	envs = append(envs, restEnvs...)
+
+	if err != nil {
+		return s.Base.Runtime.StartError(err)
+	}
+
+	// Generate the .env.local
+	s.Wool.Debug("copying special files")
+	err = templates.CopyAndApplyTemplate(ctx, shared.Embed(specialFS),
+		"templates/special/env.local.tmpl",
+		s.Local(".env.local"),
+		envs)
+	if err != nil {
+		return s.Base.Runtime.StartError(err, wool.InField("copying special files"))
+	}
+
+	// We have hot-reloading built-in
+	if s.Runner != nil {
+		s.Wool.Debug("using built-in hot reloading")
+		return s.Runtime.StartResponse()
+	}
+
+	runningContext := s.Wool.Inject(context.Background())
+	runner, err := runners.NewRunner(runningContext, "npm", "run", "dev", "--", "-p", fmt.Sprintf("%d", s.Port))
+	if err != nil {
+		return s.Base.Runtime.StartError(err, wool.InField("runner"))
+	}
+	s.Runner = runner
+	s.Runner.WithDir(s.Location)
+
+	err = s.Runner.Start()
+	if err != nil {
+		return s.Base.Runtime.StartError(err, wool.InField("runner"))
+	}
+
+	return s.Runtime.StartResponse()
+}
+
+func (s *Runtime) Information(ctx context.Context, req *runtimev0.InformationRequest) (*runtimev0.InformationResponse, error) {
+	return s.Runtime.InformationResponse(ctx, req)
+}
+
+func (s *Runtime) Stop(ctx context.Context, req *runtimev0.StopRequest) (*runtimev0.StopResponse, error) {
+	defer s.Wool.Catch()
+
+	s.Wool.Debug("stopping service")
+	err := s.Runner.Stop()
+	if err != nil {
+		return nil, s.Wool.Wrapf(err, "cannot kill go")
+	}
+
+	err = s.Base.Stop()
+	if err != nil {
+		return nil, err
+	}
+	return &runtimev0.StopResponse{}, nil
+}
+
+func (s *Runtime) Communicate(ctx context.Context, req *agentv0.Engage) (*agentv0.InformationRequest, error) {
+	return s.Base.Communicate(ctx, req)
+}
+
+/* Details
+
+ */
+
+func (s *Runtime) EventHandler(event code.Change) error {
+	s.Wool.Debug("got an event: %v")
+	return nil
+}
