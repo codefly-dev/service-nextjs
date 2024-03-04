da2f7df16e4629309a3fa8a5c7c1744fecb89eb5
chamupathi-mendis <chamupathi.mendis.cs@gmail.com>
2024-03-05 00:32:53 +0530

    move demo to routing

--- next.config.js
+++ next.config.js
@@ -2,17 +2,12 @@ module.exports = {
     async redirects() {
       return [
         // Basic redirect
-        {
-          source: '/',
-          destination: '/demo',
-          permanent: true,
-        },
-        // Wildcard path matching
-        {
-          source: '/blog/:slug',
-          destination: '/news/:slug',
-          permanent: true,
-        },
+        // {
+        //   source: '/',
+        //   destination: '/routing',
+        //   permanent: true,
+        // },
+        
       ]
     },
   }
--- templates/factory/src/pages/routing/index.tsx
+++ templates/factory/src/pages/routing/index.tsx
@@ -21,14 +21,14 @@ export const callApi = async (url) => {
   }
 };
 
-const Demo = ({ serviceEndpoints }) => {
+const Routing = ({ serviceEndpoints }) => {
   return (
     <Layout>
       <ResponseDataProvider>
         <div className="flex flex-col h-screen">
           <div className="flex flex-1">
             <div className="w-1/4 p-0 pt-0 pl-0 border-r dark:border-gray-800 ">
-              <h3 className="pl-6 mb-8">Services</h3>
+              <h2 className="pl-6 mb-8">Services</h2>
 
               <div className="overflow-y-auto" style={{ maxHeight: 'calc(100vh - 140px)' }}>
                 {serviceEndpoints.map((e) => (<Endpoint key={e.service} endpoint={{ ...e }} />))}
@@ -50,4 +50,4 @@ export async function getServerSideProps() {
   return { props: { serviceEndpoints } };
 }
 
-export default Demo;
+export default Routing;
