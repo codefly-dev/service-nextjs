429286505db3794bf62d613a0339a7036c39cefd
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-07-25 17:57:16 -0400

    feat: add cool logo

--- templates/factory/code/pages/index.tsx
+++ templates/factory/code/pages/index.tsx
@@ -1,14 +1,15 @@
-import Layout from "../components/layout";
+import Layout from '../components/layout';
+import logo from '../assets/icon-mdpi.png'; // Adjust the path to your image file
 
 const Home = () => {
-
-  return (
-    <Layout>
-      <div className="grid gap-[30px] flex items-center justify-center mt-40">
-        Codefly Home Page is coming up...
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div className="grid gap-[30px] flex items-center justify-center mt-40">
+                <h1>Welcome to Codefly</h1>
+                <img src={logo} alt="Description of the image" className="w-1/2 h-auto" />
+            </div>
+        </Layout>
+    );
 };
 
 export default Home;
